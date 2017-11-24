e8d8f2d27251da473f7aa1c80e6c04843911ea60
hadijah <hadijah315@gmail.com>
2017-11-23 21:20:34 -0800

    fixes in tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -149,6 +149,77 @@ class CategoryTestCase(unittest.TestCase):
             # drop all tables
             db.session.remove()
             db.drop_all()
+class RecipeTestCase(unittest.TestCase):
+    """This class represents the recipes test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.recipe = {'title': 'milk', 'description': 'mix well'}
+
+        # binds the app to the current context
+        with self.app.app_context():
+            # create all tables
+            db.create_all()
+
+    def test_recipe_creation(self):
+        """Test API can create a recipe (POST request)"""
+        res = self.client().post('/recipes/', data=self.recipe)
+        self.assertEqual(res.status_code, 201)
+        # self.assertIn( (res.data))
+
+    def test_api_can_get_all_recipes(self):
+        """Test API can get a recipe (GET request)."""
+        res = self.client().post('/recipes/', data=self.recipe)
+        self.assertEqual(res.status_code, 201)
+        res = self.client().get('/recipes/')
+        self.assertEqual(res.status_code, 200)
+        # self.assertIn('milk','mix well', str(res.data))
+
+    def test_api_can_get_recipe_by_id(self):
+        """Test API can get a single recipe by using it's id."""
+        rv = self.client().post('/recipes/', data=self.recipe)
+        self.assertEqual(rv.status_code, 201)
+        result_in_json = json.loads(rv.data.decode('utf-8').replace("'", "\""))
+        result = self.client().get(
+            '/recipes/{}'.format(result_in_json['id']))
+        self.assertEqual(result.status_code, 200)
+        # self.assertIn('milk','mix well', str(result.data))
+
+    def test_recipe_can_be_edited(self):
+        """Test API can edit an existing recipe. (PUT request)"""
+        rv = self.client().post(
+            '/recipes/',
+            data={'title': 'milk' , 'description':'mix well'})
+        self.assertEqual(rv.status_code, 201)
+        rv = self.client().put(
+            '/recipes/1',
+            data={
+                "title": "juice :-)", "description": " blend :-)"
+            })
+        self.assertEqual(rv.status_code, 200)
+        results = self.client().get('/recipes/1')
+        # self.assertIn('juice', 'blend',str(results.data))
+
+    def test_recipe_deletion(self):
+        """Test API can delete an existing recipe. (DELETE request)."""
+        rv = self.client().post(
+            '/recipes/',
+            data={'title': 'milk', 'description': 'mix well'})
+        self.assertEqual(rv.status_code, 201)
+        res = self.client().delete('/recipes/1')
+        self.assertEqual(res.status_code, 200)
+        # Test to see if it exists, should return a 404
+        result = self.client().get('/recipes/1')
+        self.assertEqual(result.status_code, 404)
+
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with self.app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
