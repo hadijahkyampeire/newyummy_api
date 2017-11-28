0ab0f2fb5f0e4181adf7d327c2634d6c1c53e769
hadijah <hadijah315@gmail.com>
2017-11-27 18:35:03 -0800

    adjust

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
 from app import models
 
-app = create_app(config_name=os.getenv('APP_SETTINGS'))
+app = create_app(config_name='development')
 migrate = Migrate(app, db)
 manager = Manager(app)
 
