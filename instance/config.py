fae63592730fcfc0cd861715e1807309e7ac6252
hadijah <hadijah315@gmail.com>
2017-11-27 18:38:32 -0800

    chores

--- instance/config.py
+++ instance/config.py
@@ -2,22 +2,21 @@ import os
 
 class Config(object):
     """Parent configuration class."""
-    APP_SETTINGS="development"
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = "some-very-long-string-of-random-characters-CHANGE-TO-YOUR-LIKING"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:0000@localhost/flask_api'
+    
     FLASK_APP="run.py"
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     DEBUG = True
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:0000@localhost/flask_api'
 
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI =  'postgresql://postgres:15december@localhost/test_db'
+    SQLALCHEMY_DATABASE_URI =  'postgresql://postgres:0000@localhost/test_db'
     DEBUG = True
 
 class StagingConfig(Config):
@@ -30,7 +29,6 @@ class ProductionConfig(Config):
     TESTING = False
 
 app_config = {
-    'config_name': DevelopmentConfig,
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'staging': StagingConfig,
