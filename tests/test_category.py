1ec26a4a0b43437a194af6f70b29511f54d46078
hadijahkyampeire <hadijah315@gmail.com>
2017-12-08 14:30:25 -0800

    Refactoring tests

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,6 +1,7 @@
 import unittest
 import json
 from app import create_app, db
+from app.models import Category, User, Recipe
 
 class CategoryTestCase(unittest.TestCase):
     """This class represents the category test case"""
@@ -159,47 +160,74 @@ class RecipeTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
-        self.recipe = {'title': 'milk', 'description': 'mix well'}
+        self.recipe = {'title': 'a', 'description': 'mix well' ,'category_identity': 1}
 
         # binds the app to the current context
         with self.app.app_context():
             # create all tables
             db.create_all()
 
+    def register_user(self, email="user@test.com", password="test1234"):
+        """This helper method helps register a test user."""
+        user_data = {
+            'email': email,
+            'password': password
+        }
+        return self.client().post('/auth/register', data=user_data)
+
+    def login_user(self, email="user@test.com", password="test1234"):
+        """This helper method helps log in a test user."""
+        user_data = {
+            'email': email,
+            'password': password
+        }
+        return self.client().post('/auth/login', data=user_data)
+
     def test_recipe_creation(self):
         """Test API can create a recipe (POST request)"""
         # obtain the access token
-        res = self.client().post('/categories/id/recipes', data=self.recipe)
+        self.register_user()
+        result = self.login_user()
+        # obtain the access token
+        access_token = json.loads(result.data.decode())['access_token']
+        user_id = User.decode_token(access_token)
+
+        category = Category(name='lunch', created_by=user_id)
+        category.save()
+        res = self.client().post('/categories/2/recipes', data=self.recipe)
         self.assertEqual(res.status_code, 201)
-        self.assertIn('milk', 'mix well', (res.data))
+        self.assertIn('a',str(res.data))
+        self.assertIn('mix well',str(res.data))
 
     def test_api_can_get_all_recipes(self):
         """Test API can get a recipe (GET request)."""
-        res = self.client().post('/categories/id/recipes', data=self.recipe)
+        res = self.client().post('/categories/1/recipes', data=self.recipe)
         self.assertEqual(res.status_code, 201)
 
-        res = self.client().get('/categories/<int:id>/recipes')
+        res = self.client().get('/categories/1/recipes')
         self.assertEqual(res.status_code, 200)
-        # self.assertIn('milk','mix well', str(res.data))
+        self.assertIn('a',str(res.data))
+        self.assertIn('mix well',str(res.data))
 
     def test_api_can_get_recipe_by_id(self):
         """Test API can get a single recipe by using it's id."""
-        rv = self.client().post('/categories/<int:id>/recipes/<int:recipe_id>', data=self.recipe)
+        rv = self.client().post('/categories/1/recipes/1', data=self.recipe)
         self.assertEqual(rv.status_code, 201)
         # results = json.loads(rv.data.decode())
         result = self.client().get(
-            '/recipes/id',)
+            'categories/1/recipes/1',)
         self.assertEqual(result.status_code, 200)
-        # self.assertIn('milk','mix well', str(result.data))
+        self.assertIn('a',str(result.data))
+        self.assertIn('mix well',str(result.data))
 
     def test_recipe_can_be_edited(self):
         """Test API can edit an existing recipe. (PUT request)"""
         rv = self.client().post(
-            '/categories/<int:id>/recipes/<int:recipe_id>',
-            data={'title': 'milk', 'description': 'mix well'})
+            '/categories/1/recipes/1',
+            data={'title': 'a', 'description': 'mix well'})
         self.assertEqual(rv.status_code, 201)
         rv = self.client().put(
-            '/categories/<int:id>/recipes/<int:recipe_id>',
+            '/categories/1/recipes/1',
             data={
                 "title": "juice :-)", "description": " blend :-)"
             })
