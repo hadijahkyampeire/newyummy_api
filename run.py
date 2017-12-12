9e0607cf98c211f1ac5c33ce26a72e5d351ac17b
hadijahkyampeire <hadijah315@gmail.com>
2017-12-11 23:44:24 -0800

    Add swagger

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
 from flask import render_template
+from flasgger import Swagger
 from app import create_app
 
 config_name = "development"
 app = create_app(config_name)
+swag= Swagger(app,
+   template={
+       "securityDefinitions":{
+           "TokenHeader": {
+               "type": "apiKey",
+               "name": "Authorization",
+               "in": "header"
+           }
+       }
+   })
 @app.route("/")
 def main():
     return render_template('index.html')
