ace07e73c2f4c2e8268cbab0550a8d9513299d07
HADIJAH KYAMPEIRE <hadijahkyampeire@HADIJAHs-MacBook-Pro.local>
2018-01-15 21:47:44 +0300

    Refactor views

--- app/recipes/views.py
+++ app/recipes/views.py
@@ -1,14 +1,17 @@
 from .import recipe
 from flask import request, jsonify, abort, make_response
+from sqlalchemy import or_
 from app.models import Category, User, Recipe
 from app.categories.views import is_valid, has_numbers
 from flasgger import swag_from
 from .validations import valid_recipe_title
 
+
 @recipe.route('/api/v1/categories/<int:id>/recipes', methods=['POST'])
 @swag_from('/app/docs/addrecipe.yml')
 def add_recipes(id, **kwargs):
     """This route handles posting a recipe"""
+
     auth_header = request.headers.get('Authorization')
     if auth_header is None:
         return jsonify({"message": "No token, please provide a token"}), 401
@@ -34,9 +37,7 @@ def add_recipes(id, **kwargs):
                                     category_identity=id)
                     recipe.save()
                     return recipe.json(), 201
-        else:
-            message = user_id
-            return jsonify({'message': message}), 401
+        return jsonify({'message': user_id}), 401
 
 
 @recipe.route('/api/v1/categories/<int:id>/recipes', methods=['GET'])
@@ -45,54 +46,39 @@ def get_recipes(id, **kwargs):
     """This route handles getting recipes"""
     auth_header = request.headers.get('Authorization')
     if auth_header is None:
-        return jsonify({"message": "No token, please provide a token"}),401
+        return jsonify({"message": "No token, please provide a token"}), 401
     access_token = auth_header.split(" ")[1]
     if access_token:
         user_id = User.decode_token(access_token)
         if not isinstance(user_id, str):
             page = request.args.get('page', 1, type=int)
             limit = request.args.get('limit', 3, type=int)
-            q = str(request.args.get('q', '')).lower()
+            search_query = str(request.args.get('q', '')).lower()
             identity = Category.find_user_by_id(id, user_id)
             if not identity:
-                return jsonify({"message":"You don't have"
-                                      " the recipes in that category"}), 400
-            recipes = Recipe.query.filter_by(
-                category_identity=id).paginate(page=page,
-                                            per_page=limit, error_out=False)
+                return jsonify({"message": "You don't have"
+                                " the recipes in that category"}), 400
+            recipes = Recipe.query.filter(
+                Recipe.category_identity == id)
+            if search_query:
+                recipes = recipes.filter(or_(Recipe.title.like('%' + search_query.strip().lower() + '%'),Recipe.description.like('%' + search_query.strip().lower() + '%')))
+            recipes = recipes.paginate(
+                page=page, per_page=limit, error_out=False)
             results = []
-            if q:
-                for recipe in recipes.items:
-                    if q in recipe.title.lower() or q in recipe.description:
-                        obj = recipe.json()
-                        results.append(obj)
-            else:
-                for recipe in recipes.items:
-                    obj = {
-                        'id': recipe.id,
-                        'title': recipe.title,
-                        'description': recipe.description,
-                        'date_created': recipe.date_created,
-                        'date_modified': recipe.date_modified,
-                        'category_identity': id,
-                        'Next_page': recipes.next_num,
-                        'Previous_page': recipes.prev_num,
-                        'Page_number': recipes.page
-                    }
-                    results.append(obj)
-            if len(results) <= 0:
-                return jsonify({"Error": "No recipes on that page"}), 404
+            for recipe in recipes.items:
+                results.append({
+                    'recipe': recipe.json(),
+                    'Next_page': recipes.next_num,
+                    'Previous_page': recipes.prev_num,
+                    'Page_number': recipes.page
+                })
             if results:
                 return jsonify({'recipes': results}), 200
-            else:
-                return jsonify({"message": "No recipes found"}), 404
-        else:
-            # user is not legit,an error message to handle expired token
-            message = user_id
-            return jsonify({'message': message}), 401
+            return jsonify({"message": "No recipes found"}), 404
+        return jsonify({'message': user_id}), 401
 
 
-@recipe.route('/api/v1/categories/<int:id>/recipes/<int:recipe_id>', 
+@recipe.route('/api/v1/categories/<int:id>/recipes/<int:recipe_id>',
               methods=['DELETE'])
 @swag_from('/app/docs/deleterecipe.yml')
 def delete_recipe(id, recipe_id, **kwargs):
@@ -101,24 +87,22 @@ def delete_recipe(id, recipe_id, **kwargs):
     if auth_header is None:
         return jsonify({"message": "No token, please provide a token"}), 401
     access_token = auth_header.split()[1]
-    if access_token :
+    if access_token:
         user_id = User.decode_token(access_token)
         if not isinstance(user_id, str):
             identity = Category.find_user_by_id(id, user_id)
-            if not identity :
+            if not identity:
                 return jsonify({"message": "You don't have"
                                 " that recipe in that category"}), 400
             recipe = Recipe.find_recipe_by_id(recipe_id, id)
-            if not recipe :
+            if not recipe:
                 return jsonify({"message": "No recipes with"
                                 " that id to delete "}), 404
-            else:
-                recipe.delete()
-                return {"message": "recipe {} deleted"
-                               " successfully".format(recipe.id)}, 200
-        else:
-            message = user_id
-            return jsonify({'message': message}), 401
+      
+            recipe.delete()
+            return {"message": "recipe {} deleted"
+                    " successfully".format(recipe.id)}, 200
+        return jsonify({'message': user_id}), 401
 
 
 @recipe.route('/api/v1/categories/<int:id>/recipes/<int:recipe_id>',
@@ -136,7 +120,7 @@ def edit_recipe(id, recipe_id, **kwargs):
             title = str(request.data.get('title', '')).strip()
             description = str(request.data.get('description', '')).strip()
             identity = Category.find_user_by_id(id, user_id)
-            if not identity :
+            if not identity:
                 return jsonify({"message": "You don't have"
                                 " that recipe in that category"}), 400
             result2 = valid_recipe_title(title)
@@ -147,24 +131,20 @@ def edit_recipe(id, recipe_id, **kwargs):
             if result:
                 return jsonify({"message": "Recipe already exists"}), 400
             recipe = Recipe.find_recipe_by_id(recipe_id, id)
-            if not recipe :
-                return jsonify({"message":"No recipes"
-                                          " with that id to edit "}), 404
-            else:
-                title = str(request.data.get('title', '')).lower()
-                description = str(request.data.get('description', ''))
-                recipe.title = title
-                recipe.description = description
-                recipe.save()
-                return recipe.json(), 200
-        else:
-            # user is not legit, an error message to handle expired token
-            message = user_id
-            return jsonify({'message': message}), 401
+            if not recipe:
+                return jsonify({"message": "No recipes"
+                                " with that id to edit "}), 404
+            title = str(request.data.get('title', '')).lower()
+            description = str(request.data.get('description', ''))
+            recipe.title = title
+            recipe.description = description
+            recipe.save()
+            return recipe.json(), 200
+        return jsonify({'message': user_id}), 401
 
 
-@recipe.route('/api/v1/categories/<int:id>/recipes/<int:recipe_id>', 
-               methods=['GET'])
+@recipe.route('/api/v1/categories/<int:id>/recipes/<int:recipe_id>',
+              methods=['GET'])
 @swag_from('/app/docs/getrecipe.yml')
 def get_recipe_by_id(id, recipe_id, **kwargs):
     """This route handles getting a recipe by id"""
@@ -178,14 +158,10 @@ def get_recipe_by_id(id, recipe_id, **kwargs):
             identity = Category.find_user_by_id(id, user_id)
             if not identity:
                 return jsonify({"message": "You don't have"
-                               " that recipe in that category"}), 400
+                                " that recipe in that category"}), 400
             recipe = Recipe.find_recipe_by_id(recipe_id, id)
             if not recipe:
-                return jsonify({"message":"No recipes with"
-                                          " that id to get"}), 400
-            else:
-                return recipe.json(), 200
-        else:
-            # user is not legit,an error message to handle expired token
-            message = user_id
-            return jsonify({'message': message}), 401
+                return jsonify({"message": "No recipes with"
+                                " that id to get"}), 400
+            return recipe.json(), 200
+        return jsonify({'message': user_id}), 401
